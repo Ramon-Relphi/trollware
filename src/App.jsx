836ec51f80d587cb810aa74af8ee9c5f1cd653d3
Ramon <168929344+Ramon-Relphi@users.noreply.github.com>
2024-06-11 17:57:43 +0000

    Fix export and import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Boot from './pages/Boot/Boot'
 import Console from './pages/Console/Console'
 import Infected from './pages/Infected/Infected'
 
-import { Pages } from './constants/index'
+import { Pages } from './constants'
 import { getStorageValue } from './services/storage/storage'
 
 function App() {
